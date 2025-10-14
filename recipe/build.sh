4660b0d50457783d241f0ef99f5677705ab93f82
Phil Chiu <pchiu@archer.com>
2025-10-14 17:33:12 -0600

    Patch daft to build dashboard components with node

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,5 @@ set -euo pipefail
 export OPENSSL_DIR=${PREFIX}
 export OPENSSL_NO_VENDOR=1
 
-# build dashboard assets using bun
-cd daft
-pushd ./src/daft-dashboard/frontend
-${SRC_DIR}/bun/bun install
-${SRC_DIR}/bun/bun run build
-popd
-
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
