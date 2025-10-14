879efe94da55f4fc24cd287286a74f9f00b985b1
Phil Chiu <pchiu@archer.com>
2025-10-14 17:44:26 -0600

    Don't need additional folder layer anymore

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,5 @@
 set OPENSSL_DIR=%LIBRARY_PREFIX%
 set OPENSSL_NO_VENDOR=1
 
-REM build dashboard assets using bun
-cd daft
-pushd .\src\daft-dashboard\frontend
-"%SRC_DIR%\bun\bun" install
-"%SRC_DIR%\bun\bun" run build
-popd
-
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
