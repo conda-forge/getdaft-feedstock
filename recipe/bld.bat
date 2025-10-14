771cc16fca069b429c2009441a791360900a4761
Phil Chiu <pchiu@archer.com>
2025-10-14 17:47:17 -0600

    Simplify meta.yaml again

--- recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-set OPENSSL_DIR=%LIBRARY_PREFIX%
-set OPENSSL_NO_VENDOR=1
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
--- recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-export OPENSSL_DIR=${PREFIX}
-export OPENSSL_NO_VENDOR=1
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
