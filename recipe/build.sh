ec151351642bcded414dc6bd649bb269edcedf6d
Phil Chiu <whophil@users.noreply.github.com>
2025-11-05 23:16:33 -0700

    fix all CFLAGS*

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,11 @@ npm run build
 popd
 
 # aws-lc-sys jitterentropy raises when compiled with -02
-export CFLAGS="${CFLAGS//-O2/-O0}"
+for var in $(env | grep -o '^CFLAGS[^=]*'); do
+    eval "export $var=\"$(eval echo \$$var | sed 's/-O[0-3]/-O0/g')\""
+done
+
+# export AWS_LC_SYS_NO_JITTER_ENTROPY=1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
